c9a4d338bbf68d9a1648b31c596d4176a9c96683
Naveez Munir <naveezmunir@gmail.com>
2024-09-15 00:00:12 +0500

    configure theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      height: {
+        "80vh": "80vh",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        borderColor: "var(--border-color)",
+        shadowColor: "var(--shadow-color)",
+        accent: "var(--accent)",
+      },
+      boxShadow: {
+        custom: "0 4px 6px var(--shadow-color)",
+      },
+      borderColor: {
+        custom: "var(--border-color)",
       },
     },
   },
